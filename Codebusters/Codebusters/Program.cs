4ec43b9ecf6551fe047678350651840535c62605
fonxy6 <viktor_6@windowslive.com>
2023-10-18 12:13:40 +0200

    CORS added to Program.cs

--- Codebusters/Codebusters/Program.cs
+++ Codebusters/Codebusters/Program.cs
@@ -1,5 +1,7 @@
 using Codebusters.Service;
 
+const string myAllowSpecificOrigins = "_myAllowSpecificOrigins";
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -10,7 +12,18 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddSingleton<IUserRepository, UserRepository>();
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: myAllowSpecificOrigins,
+        policy  =>
+        {
+            policy.WithOrigins("http://localhost:3000");
+        });
+});
+
 var app = builder.Build();
+
+app.UseCors(myAllowSpecificOrigins);
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
