6e9587bd8466025084aec41b68c6bc666f9e6a5e
K Balázs <kutbal87@gmail.com>
2023-12-12 14:59:06 +0100

    Authorization + Pass change

--- Codebusters/Codebusters/Controllers/UserController.cs
+++ Codebusters/Codebusters/Controllers/UserController.cs
@@ -1,4 +1,5 @@
-﻿using Codebusters.Data;
+﻿using Codebusters.Contracts;
+using Codebusters.Data;
 using Codebusters.Model;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
@@ -57,11 +58,77 @@ public class UserController : ControllerBase
         }
     }
     
-    [HttpDelete("deleteAccount")]
-    [Authorize(Roles = "User, Leader")]
+    [HttpGet("/getOwnUsers"), Authorize(Roles = "Leader")]
+    public ActionResult<IEnumerable<Tuple<User, IdentityUser>>> GetOwnUsers(string companyName)
+    {
+        try
+        {
+            var returningList = new List<Tuple<User, IdentityUser>>();
+
+            var users = _userContext?.UsersDb!.Where(cn => cn.CompanyNameByDatabase == companyName || cn.CompanyNameByDatabase == "Not added yet");
+            if (users == null || !users.Any())
+            {
+                _logger.LogInformation("There is no user in the database.");
+                return Ok(users);
+            }
+
+            var identityUsers = _userContext?.Users.ToList();
+
+            foreach (var user in users)
+            {
+                var identityUser = identityUsers.FirstOrDefault(identityUser => user.IdentityUserId == identityUser.Id);
+                if (identityUser != null)
+                {
+                    var tuple = new Tuple<User, IdentityUser>(user, identityUser);
+                    returningList.Add(tuple);
+                }
+            }
+            
+            return Ok(returningList);
+        }
+        catch (Exception e)
+        {
+            _logger.LogError(e.ToString());
+            return NotFound();
+        }
+    }
+    
+    [HttpPatch("ChangePassword"), Authorize(Roles = "User")]
+    public async Task<ActionResult<ChangePasswordResponse>> ChangePassword([FromBody] ChangePasswordRequest request)
+    {
+        try
+        {
+            var existingUser = await _userManager.FindByEmailAsync(request.Email);
+            if (existingUser == null)
+            {
+                return BadRequest(existingUser);
+            }
+
+            var result = await _userManager.ChangePasswordAsync(existingUser, request.CurrentPassword, request.NewPassword);
+
+            await _userContext.SaveChangesAsync();
+
+            if (result.Succeeded)
+            {
+                await _userContext.SaveChangesAsync();
+                return Ok($"Successful pass change {request.Email}");
+            }
+            else
+            {
+                return BadRequest($"Error pass change {request.Email}");
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            return NotFound("Error delete sun data");
+        }
+    }
+    
+    [HttpDelete("deleteAccount"), Authorize(Roles = "User, Leader")]
     public async Task<ActionResult<string>> DeleteAccount(string email)
     {
-        if (_config["AEmail"].ToLower() == email.ToLower())
+        if (_config["AEmail"]!.ToLower() == email.ToLower())
         {
             return Unauthorized();
         }
