79eff88327560805da21bdfc8dcdc9f50b0185bc
fonxy6 <viktor_6@windowslive.com>
2023-12-13 12:18:28 +0100

    construct table working properly with the pagination

--- frontend/src/Components/ConstructTable/ConstructTable.jsx
+++ frontend/src/Components/ConstructTable/ConstructTable.jsx
@@ -1,22 +1,22 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { StyledUl, StyledLi } from "../../Styles/Table.Styles"
+import { useNavigate } from 'react-router-dom';
 
-const ConstructTable = ({constructs}) => {
+const ConstructTable = ({constructs, page}) => {
   const [recordPerPage, setRecordPerPage] = useState(10);
-  const [paginationSlice, setPaginationSlice] = useState({first: 0, second: recordPerPage});
+  const [paginationSlice, setPaginationSlice] = useState({first: Number(page) * recordPerPage - recordPerPage, second: Number(page) * recordPerPage -1});
+  const navigate = useNavigate();
 
+  useEffect(() => {
+    setPaginationSlice({first: Number(page) * recordPerPage - recordPerPage, second: Number(page) * recordPerPage -1});
+  }, [page]);
+  
   const handlePreviousButton = () => {
-    setPaginationSlice((prevSlice) => ({
-      first: Number(prevSlice.first) - recordPerPage,
-      second: Number(prevSlice.second) - recordPerPage
-    }));
+    navigate(`/constructs/${Number(page) - 1}`)
   }
 
   const handleNextButton = () => {
-    setPaginationSlice((prevSlice) => ({
-      first: Number(prevSlice.first) + recordPerPage,
-      second: Number(prevSlice.second) + recordPerPage
-    }));
+    navigate(`/constructs/${Number(page) + 1}`)
   }
 
   const handleRecordPerPageButton = (number) => {
@@ -65,7 +65,7 @@ const ConstructTable = ({constructs}) => {
           </tr>
         </thead>
         <tbody>
-           {constructs && constructs?.slice(paginationSlice.first, paginationSlice.second).map((construct, index) => (
+           {constructs && constructs.slice(paginationSlice.first, paginationSlice.second).map((construct, index) => (
             <tr key={construct.id} style={{ backgroundColor: index % 2 === 1 ? '#f2f2f2' : '' }}>
               <td>{construct.name}</td>
               <td>{construct.companyName}</td>
@@ -77,10 +77,14 @@ const ConstructTable = ({constructs}) => {
       </table>
       <nav aria-label="Page navigation example">
         <StyledUl  className="pagination">
-          <li className="page-item"><a className="page-link" onClick={() => handlePreviousButton()}>Previous</a></li>
-          <li className="page-item"><a className="page-link"  onClick={() => handleNextButton()}>Next</a></li>
+          <li className="page-item">
+            <button className="btn btn-primary btn-floating mb-4"  style={{marginLeft: "1vw"}} onClick={() => handlePreviousButton()} disabled={paginationSlice.first === 0}>Previous</button>
+          </li>
+          <li className="page-item">
+            <button className="btn btn-primary btn-floating mb-4"  style={{marginLeft: "1vw"}} onClick={() => handleNextButton()} disabled={constructs && paginationSlice.second > constructs.length} >Next</button>
+          </li>
           <StyledLi>
-            <select className="page-link" onChange = {(event) => handleRecordPerPageButton(event.target.value)}>
+            <select className="btn btn-primary"  style={{marginRight: "1vw"}} onChange = {(event) => handleRecordPerPageButton(event.target.value)}>
               <option>10</option>
               <option>25</option>
               <option>50</option>
--- frontend/src/Pages/ConstructList.jsx
+++ frontend/src/Pages/ConstructList.jsx
@@ -2,11 +2,13 @@ import { useEffect, useState } from "react";
 import Loading from "../Components/Loading";
 import ConstructTable from "../Components/ConstructTable";
 import useFetch from "../Hooks/useFetch";
+import { useParams } from 'react-router-dom';
 
 const ConstructList = () => {
   const [loading, setLoading] = useState(true);
   const [constructs, setConstructs] = useState(null);
   const constr = useFetch("getConstructs");
+  const { page } = useParams();
   
   useEffect(() => {
       setConstructs(constr);
@@ -20,6 +22,7 @@ const ConstructList = () => {
       ) : (
         <ConstructTable
           constructs = { constructs }
+          page = { page }
         />
       )}
     </div>
